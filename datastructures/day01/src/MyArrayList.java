d21f8ddeb46b383311b635c8c846b8c11866c3a0
coverney <cassandra.overney@students.olin.edu>
2018-01-28 21:42:56 -0500

    Day 1 HW: ArrayLists

--- datastructures/day01/src/MyArrayList.java
+++ datastructures/day01/src/MyArrayList.java
@@ -1,42 +1,222 @@
+import static java.lang.Math.abs;
+
 public class MyArrayList {
     private Cow[] elems;
     private int size;
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(1)
     public MyArrayList() {
-        // TODO
+        this(10);
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(n) or 0(1)?
     public MyArrayList(int capacity) {
-        // TODO
+        this.elems = new Cow[capacity];
+        this.size = 0;
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(1)*
     public void add(Cow c) {
-        // TODO
+        if (size >= 0.25 * elems.length) {
+            Cow[] elems2 = new Cow[elems.length * 2];
+            System.arraycopy(elems, 0, elems2, 0, elems.length);
+            elems = elems2;
+        }
+
+        this.elems[size] = c;
+        size++;
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(1)
     public int size() {
-        // TODO
-        return -1;
+        return this.size;
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(1)
     public Cow get(int index) {
-        // TODO
-        return null;
+        if (index < 0 || index >= this.size) {
+
+            throw new IndexOutOfBoundsException("index is out of array bounds");
+        }
+
+        Cow cow = this.elems[index];
+        return cow;
+
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(n)*
     public Cow remove(int index) {
-        // TODO
-        return null;
+        if (index < 0 || index >= this.size) {
+
+            throw new IndexOutOfBoundsException("index is out of array bounds");
+        }
+        Cow cow = this.elems[index];
+        // shift the array to the left
+        for (int i = index; i < size - 1; i++) {
+
+            elems[i] = elems[i + 1];
+        }
+        this.size--;
+
+        if (size < 0.25 * elems.length) {
+            Cow[] elems2 = new Cow[elems.length / 2];
+            System.arraycopy(elems, 0, elems2, 0, elems.length / 2);
+            elems = elems2;
+        }
+        return cow;
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(n)
     public void add(int index, Cow c) {
-        // TODO
+        if (index < 0 || index > this.size) {
+            throw new IndexOutOfBoundsException("index is out of array bounds");
+        }
+        this.add(c);
+        size--;
+
+        for (int i = size; i > index; i--) {
+            elems[i] = elems[i - 1];
+        }
+        elems[index] = c;
+        size++;
+    }
+
+    // Additional Practice
+
+    //Given an array with an odd number of elements, find the median without sorting.
+    //Runtime: O(n2)
+    public int find_median(int[] array) throws Exception {
+        for (int i = 0; i < array.length - 1; i++) {
+            int num_bigger = 0;
+            int num_smaller = 0;
+            int num_equal = 0;
+            for (int j = 0; j < array.length; j++) {
+
+                if (array[i] > array[j]) {
+                    num_bigger++;
+                } else if (array[i] < array[j]) {
+                    num_smaller++;
+                } else {
+                    num_equal++;
+                }
+            }
+
+            if (abs(num_bigger - num_smaller) < num_equal) {
+                return array[i];
+            }
+
+        }
+        throw new Exception("not odd");
+    }
+
+    public int circularly_sorted(int[] array) {
+        if (array.length < 1) {
+            return -1;
+        }
+
+        int num_descending = 0;
+        int index = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]){
+                num_descending = num_descending + 1;
+                index = i;
+            }
+
+            if((num_descending > 1)|| (num_descending > 0 && array[index-1] < array[array.length-1])){
+                return -1;
+            }
+        }
+
+        return index;
+    }
+
+    //Find the longest sorted substring of an input array. Return an array of size 2: the starting and ending indices of said substring.
+    public int[] longest_sorted(int[] array){
+        int[] descending = new int[array.length];
+        int[] ascending = new int[array.length];
+        for (int i = 1; i < array.length; i++) {
+
+            if(array[i] > array[i-1]){
+                ascending[i] = 1;
+
+            }
+            else if(array[i] < array[i-1]){
+                descending[i] = 1;
+
+            }
+            else{
+                ascending[i] = 1;
+                descending[i] = 1;
+            }
+        }
+
+        int max_inrow1 = 0;
+        int max_index1 = 0;
+        int temp_inrow1 = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(ascending[i] == 1){
+                temp_inrow1++;
+            }
+            if(ascending[i] == 0){
+                if (temp_inrow1 > max_inrow1){
+                    max_inrow1 = temp_inrow1;
+                    max_index1 = i-1;
+                    temp_inrow1 = 0;
+                }
+            }
+        }
+
+        if (temp_inrow1 > max_inrow1){
+            max_inrow1 = temp_inrow1;
+            max_index1 = array.length-1;
+        }
+
+        int max_inrow2 = 0;
+        int max_index2 = 0;
+        int temp_inrow2 = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(descending[i] == 1){
+                temp_inrow2++;
+            }
+            if(descending[i] == 0){
+                if (temp_inrow2 > max_inrow2){
+                    max_inrow2 = temp_inrow2;
+                    max_index2 = i-1;
+                    temp_inrow2 = 0;
+                }
+            }
+        }
+        if (temp_inrow2 > max_inrow2){
+            max_inrow2 = temp_inrow2;
+            max_index2 = array.length-1;
+        }
+
+        if (max_inrow1 >= max_inrow2){
+            return new int[]{array[max_index1 - (max_inrow1)], array[max_index1]};
+        }
+        else{
+            return new int[]{array[max_index2 - (max_inrow2)], array[max_index2]};
+        }
+    }
+
+    public int[] longest_sorted2(int[] array){
+        int start = 0;
+        int end = 0;
+
+        int curr_start = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] >= array[i-1]){
+                if (i - curr_start > end - start){
+                    start = curr_start;
+                    end = i;
+                }
+            }
+            else{
+                curr_start = i;
+            }
+        }
+        return new int[]{array[start], array[end]};
     }
 }
--- datastructures/day01/test/MyArrayListTest.java
+++ datastructures/day01/test/MyArrayListTest.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+//import static junit.framework.TestCase.assertEquals;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -87,5 +91,57 @@ public class MyArrayListTest {
         assertEquals(1000,resizeCows.size());
         assertEquals(new Cow("Cow996", 996, "Color996"),resizeCows.get(996));
     }
+
+    @Test
+    public void testMedian() throws Exception {
+        int[] array1 = new int[]{1, 5, 7, 8, 4};
+        int[] array2 = new int[]{1, 2, 1, 2, 3};
+        assertEquals(5,cows.find_median(array1));
+        assertEquals(2,cows.find_median(array2));
+
+    }
+
+    @Test
+    public void testCircularly_sorted() throws Exception {
+        int[] array1 = new int[]{5, 4, 3, 1, 2};
+        int[] array2 = new int[]{8, 1, 4, 5, 6};
+        int[] array3 = new int[]{8, 9, 10, 1, 4};
+        int[] array4 = new int[]{5, 1, 4, 5, 6};
+
+        assertEquals(-1,cows.circularly_sorted(array1));
+        assertEquals(1,cows.circularly_sorted(array2));
+        assertEquals(3,cows.circularly_sorted(array3));
+        assertEquals(-1,cows.circularly_sorted(array4));
+
+    }
+
+    @Test
+    public void testlongest_sorted() throws Exception {
+        int[] array1 = new int[]{5, 4, 3, 1, 2};
+        int[] array2 = new int[]{8, 1, 4, 5, 6};
+        int[] array3 = new int[]{8, 9, 10, 1, 4};
+        int[] array4 = new int[]{5, 1, 4, 5, 6};
+
+        Assert.assertArrayEquals(new int[]{5,1},cows.longest_sorted(array1));
+        Assert.assertArrayEquals(new int[]{1,6},cows.longest_sorted(array2));
+        Assert.assertArrayEquals(new int[]{8,10},cows.longest_sorted(array3));
+        Assert.assertArrayEquals(new int[]{1,6},cows.longest_sorted(array4));
+
+    }
+
+//    @Test
+//    public void testlongest_sorted2() throws Exception {
+//        int[] array1 = new int[]{5, 4, 3, 1, 2};
+//        int[] array2 = new int[]{8, 1, 4, 5, 6};
+//        int[] array3 = new int[]{8, 9, 10, 1, 4};
+//        int[] array4 = new int[]{5, 1, 4, 5, 6};
+//
+//        //Assert.assertArrayEquals("hi", new int[]{5,1},cows.longest_sorted2(array1));
+////        Arrays.equals(new int[]{5,1},cows.longest_sorted2(array1));
+////        Arrays.equals(new int[]{1,6},cows.longest_sorted2(array2));
+////        Arrays.equals(new int[]{8,10},cows.longest_sorted2(array3));
+////        Arrays.equals(new int[]{1,6},cows.longest_sorted2(array4));
+//
+//    }
 
 }
