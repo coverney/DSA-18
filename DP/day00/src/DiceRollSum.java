7ae8b82960b6aa14570d0c7ed3bc77f2d04661ac
coverney <cassandra.overney@students.olin.edu>
2018-04-22 20:11:53 -0400

    finished day 0 of DP

--- DP/day00/src/DiceRollSum.java
+++ DP/day00/src/DiceRollSum.java
@@ -1,10 +1,31 @@
 public class DiceRollSum {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(N*D)
+    // Space: O(N) not including space it takes for recursive calls
     public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+        int[] DP = new int[N + 1];
+        for (int i = 0; i < DP.length; i++) {
+            DP[i] = -1; // set a special empty value
+        }
+        return diceRecurs(N, DP);
+    }
+
+    private static int diceRecurs(int i, int[] DP) {
+        // base cases
+        if (i < 0) return 0;
+        if (i == 0) return 1;
+
+        // have we already solved this subproblem
+        if (DP[i] != -1) return DP[i];
+
+        // DP[i] = sum for k=1 to 6 of DP[N-k]
+        int answer = 0;
+        for (int j = 1; j < 7; j++) {
+            answer += diceRecurs(i - j, DP);
+        }
+        // store our answer and return it
+        DP[i] = answer;
+        return answer;
     }
 
 }
--- DP/day00/src/LongestIncreasingSubsequence.java
+++ DP/day00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,53 @@
-public class LongestIncreasingSubsequence {
+import java.util.Arrays;
 
-    // Runtime: TODO
-    // Space: TODO
+public class LongestIncreasingSubsequence {
+    public static int max;
+    // Runtime: O(N^2) must do sub problem around N times, for each sub problem need to iterate through more problems to find max
+    // Space: O(N) not including space it takes for recursive calls
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        LongestIncreasingSubsequence.max = 0;
+
+        int[] DP = new int[A.length];
+        for (int i = 0; i < DP.length; i++) {
+            DP[i] = -1; // set a special empty value
+        }
+
+        subseqRecurs(A, DP, A.length);
+        return LongestIncreasingSubsequence.max;
+    }
+
+    private static int subseqRecurs(int[] input, int[] DP, int index) {
+        // base cases
+        if (index == 0){
+            return 0;
+        }
+        if (index == 1){
+            return 1;
+        }
+
+        // check memo
+        if (DP[index-1] != -1){
+            return DP[index-1];
+        }
+
+        // find the max LIS for a sub sequence from 0 to index
+        int res, max_LIS_here = 1;
+
+        for (int i = 1; i < index; i++) {
+            // max LIS for sub problem is just 1 + the max LIS of an earlier sequence that it would fit in
+            res = subseqRecurs(input, DP, i);
+            if (input[i-1] < input[index-1] && res + 1 > max_LIS_here){
+                max_LIS_here = res + 1;
+            }
+        }
+
+        // update the global max
+        if (LongestIncreasingSubsequence.max < max_LIS_here){
+            LongestIncreasingSubsequence.max = max_LIS_here;
+        }
+
+        // update the memo and return
+        DP[index-1] = max_LIS_here;
+        return max_LIS_here;
     }
 }
